f696becebeab8031da7ed7a4a9ba3df6e8dee9e1
wasieDev <127023454+rens20@users.noreply.github.com>
2023-12-16 18:20:16 +0800

    Update main.jsx
    Signed-off-by: wasieDev <127023454+rens20@users.noreply.github.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './src/App'
+import App from './App'
 import './index.css'
-import './Calculate.jsx'
+import './calculate'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
